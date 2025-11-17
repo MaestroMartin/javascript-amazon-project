283b84cb4b47852f02dfabeb76bfdf7124157b5b
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2025-11-17 14:26:29 +0100

    moove addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,23 @@
 
 export const cart=[];
+
+export function addtoCart(productId) {
+    let matchedItem;
+
+    cart.forEach((cartItem) => {
+
+        if (productId === cartItem.productId) {
+            matchedItem = cartItem;
+        }
+    });
+
+    if (matchedItem) {
+        matchedItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+
+};
