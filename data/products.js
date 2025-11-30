7aafaa426a01b395c5422b2ce39c4b8bc467f6b8
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2025-11-30 15:38:45 +0100

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,6 @@
+import { formatCurency } from "../scripts/utils/money.js";
+
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -9,7 +12,31 @@ export function getProduct(productId) {
 
   return matchingProduct;
 }
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
 
+  constructor(productDetails){
+    this.id= productDetails.id;
+    this.image= productDetails.image;
+    this.name= productDetails.name;
+    this.rating= productDetails.rating;
+    this.priceCents= productDetails.priceCents;
+    this.keywords= productDetails.keywords;
+  }
+  
+  getStarsURL(){
+    return`images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `$${formatCurency(this.priceCents )}`;
+  }
+}
 
 export const products = [
   {
@@ -670,4 +697,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=> {
+    return new Product (productDetails);
+  });
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkOut/OrderSummary";
 import { renderPaymentSummary } from "./checkOut/paymentSummary.js";
-import "../data/cart-class.js";
+//import "../data/cart-class.js";
 
 renderOrderSummary();
 renderPaymentSummary();
